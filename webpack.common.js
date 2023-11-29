b294a685df87166e50929a771c6b05ed200de3a3
Jose Fernando Huarez Reyes <jose.huarezr@gmail.com>
2023-11-28 22:06:06 -0500

    barebones project

---
+++ webpack.common.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+            filename: 'index.html',
+            title: 'Restaurant Page',
+            inject: 'body'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader' // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
+    ]
+  }
+  
+};
